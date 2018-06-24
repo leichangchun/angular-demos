ab4f309e13553fa7f94169396c74c71a7284236f
leichangchun <springhubei@163.com>
2018-06-25 00:16:08 +0800

    sync

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl , Validators, FormGroup , FormBuilder, ValidatorFn} from '@angular/forms';
+
 @Component({
   selector: 'app-login2',
   templateUrl: './login2.component.html',
@@ -7,14 +8,51 @@ import { FormControl } from '@angular/forms';
 })
 export class Login2Component implements OnInit {
 
-  public name = new FormControl();
-  constructor() { }
+  // public name = new FormControl('' , {
+  //   validators : Validators.required,
+  //   updateOn : 'blur'
+  // });
+
+  // public loginForm = new FormGroup({
+  //   name : new FormControl('', Validators.maxLength(20)),
+  //   psw : new FormControl('')
+  // }, Validators.required);
+
+  public loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    // this.loginForm = fb.group({
+    //   name : ['' , Validators.required],
+    //   psw : ['']
+    // });
+    this.loginForm = fb.group({
+      name: this.fb.group({
+        firstname: ['' , Validators.required],
+        lastname: ['']
+      }, this.sameName()),
+      psw: ['' , Validators.required]
+    });
+  }
+
+
+  sameName (): ValidatorFn {
+    return (control: FormGroup): { [key: string]: any} => {
+      console.log(control.get('firstname'));
+      return '' === '' ? { 'sameName' : true } : null;
+    };
+  }
 
   ngOnInit() {
   }
 
   login () {
-    console.log(this.name);
+
+  }
+
+  getFormControl(name: string) {
+    return this.loginForm.get(name);
   }
 
 }
